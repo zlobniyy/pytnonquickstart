3cf780cde32ade0f8ff10674005c8fe487a3ea58
Кравченко Степан Борисович <skravchenko@win.ftc.ru>
2017-01-16 15:15:56 +0700

    - сделан цикл для меню действий

--- study1.py
+++ study1.py
@@ -5,40 +5,45 @@ import shutil
 
 def work_func():
     print("Это похвально!")
-    print("Выберите действие:")
-    print("[1] - Показать содержимое указанного каталога")
-    print("[2] - Скопировать указанный файл *.dupl")
-    print("[3] - удалить дубликаты в указанном каталоге *.dupl")
-    action=str(input())
-    if action == '1':
-        dirname=input("Укажите путь до каталога: ")
-        i=""
-        list=os.listdir(dirname)
-        for i in list:
-            if os.path.isfile(os.path.join(dirname,i)):
-                print(i)
-    elif action == '2':
-        filename=input("Укажите путь до файла: ")
-        newfilename=filename+".dupl"
-        shutil.copy(filename,newfilename)
-        if os.path.exists(newfilename):
-            print("Файл "+filename+" скопирован в "+newfilename)
-        else:
-            print("Возникли проблемы при копировании "+filename)
-    elif action == '3':
-        k=0
-        dirname = input("Укажите путь до каталога: ")
-        list=os.listdir(dirname)
-        for i in list:
-            if i.endswith(".dupl"):
-                print("файл подходит и будет удален: "+i )
-                os.remove(os.path.join(dirname,i))
-                k +=1
+    action=""
+    while action !="q":
+        print("Выберите действие:")
+        print("[1] - Показать содержимое указанного каталога")
+        print("[2] - Скопировать указанный файл *.dupl")
+        print("[3] - удалить дубликаты в указанном каталоге *.dupl")
+        print("[q] - выйти в меню выше")
+        action=str(input())
+        if action == '1':
+            dirname=input("Укажите путь до каталога: ")
+            i=""
+            list=os.listdir(dirname)
+            for i in list:
+                if os.path.isfile(os.path.join(dirname,i)):
+                    print(i)
+        elif action == '2':
+            filename=input("Укажите путь до файла: ")
+            newfilename=filename+".dupl"
+            shutil.copy(filename,newfilename)
+            if os.path.exists(newfilename):
+                print("Файл "+filename+" скопирован в "+newfilename)
             else:
-                print("файл НЕподходит, пропускаем: " + i)
-            print("Удалено "+str(k)+" файлов.")
-    else:
-        print("Вы выбрали что-то непонятное")
+                print("Возникли проблемы при копировании "+filename)
+        elif action == '3':
+            k=0
+            dirname = input("Укажите путь до каталога: ")
+            list=os.listdir(dirname)
+            for i in list:
+                if i.endswith(".dupl"):
+                    print("файл подходит и будет удален: "+i )
+                    os.remove(os.path.join(dirname,i))
+                    k +=1
+                else:
+                    print("файл НЕподходит, пропускаем: " + i)
+                print("Удалено "+str(k)+" файлов.")
+        elif action=="q":
+            pass
+        else:
+            print("Вы выбрали что-то непонятное")
 
 print("Это первая программа.")
 print("Здравствуйте, Пользователь.")
